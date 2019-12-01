f964c994e8ee6344c40f4ad46bfcfe94bcdeec39
gaohx <1109546392@qq.com>
2019-12-01 17:20:01 +0800

    需求1

---
+++ src/main/java/com/jack/demo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.jack.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/01")
+    public String test01() {
+        return "test01";
+    }
+}
